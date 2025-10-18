f6f4c80f542b5c4094ba38097f989ce4969890e9
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-10-18 14:42:45 -0300

    feat: Added Entities and refactored Domain Models
    Moved Document and Media models to the 'document' package, replaced old User and Document models with updated versions, and introduced new domain models for donation, logistics, process, and sale. Added supporting enums and a TimeStampedEntity base class for auditing. Updated repository and controller imports to reflect new package structure.

---
+++ src/main/java/org/code/api/domain/common/TimeStampedEntity.java
@@ -0,0 +1,44 @@
+package org.code.api.domain.common;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.code.api.domain.models.user.User;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class TimeStampedEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "criador_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "usuario_criador_id_fk",
+                    foreignKeyDefinition = "FOREIGN KEY (criador_id) REFERENCES usuario (id) ON DELETE RESTRICT ON UPDATE CASCADE"
+            ),
+            nullable = false)
+    private User createdBy;
+
+    @Column(name = "data_criacao", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "data_atualizacao", nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
--- src/main/java/org/code/api/dto/document/DocumentResponseDTO.java
+++ src/main/java/org/code/api/dto/document/DocumentResponseDTO.java
@@ -1,11 +1,12 @@
 package org.code.api.dto.document;
 
-import org.code.api.domain.models.Document;
+import org.code.api.domain.models.document.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
-public record DocumentResponseDTO(UUID id, String nome, Date datadDocumento, Date dataInsercao) {
+public record DocumentResponseDTO(UUID id, String nome, LocalDateTime datadDocumento, LocalDateTime dataInsercao) {
     public DocumentResponseDTO(Document document) {
         this(document.getId(), document.getNome(), document.getDataDocumento(), document.getDataInsercao());
     }
