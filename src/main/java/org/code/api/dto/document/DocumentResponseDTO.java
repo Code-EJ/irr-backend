490ddd2a2b0ade7cca0bd8f7c9166b56dc88e65b
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-10-20 20:04:27 -0300

    fix: remove unused imports

--- src/main/java/org/code/api/dto/document/DocumentResponseDTO.java
+++ src/main/java/org/code/api/dto/document/DocumentResponseDTO.java
@@ -3,7 +3,6 @@ package org.code.api.dto.document;
 import org.code.api.domain.models.document.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 public record DocumentResponseDTO(UUID id, String nome, LocalDateTime datadDocumento, LocalDateTime dataInsercao) {
