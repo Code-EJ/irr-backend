00abfcf0cfdcfd0a28f0e745085cdbdadfac0642
tosuki <okaabe2006@gmail.com>
2025-12-27 21:19:20 -0300

    chore: remove useless import

--- src/main/java/org/code/api/domain/models/user/Session.java
+++ src/main/java/org/code/api/domain/models/user/Session.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import org.code.api.domain.enums.UserType;
 
 @Builder
