11c56d73c74b5b09556f6656c1a26212648f86cb
PDornn <ped.henriquedornelas@gmail.com>
2026-05-17 07:06:55 -0300

    Correção de inconsistencias que houveram na hora de rodar

--- src/main/java/org/code/api/dto/logistic/vehicle/request/VehicleCreateRequestDTO.java
+++ src/main/java/org/code/api/dto/logistic/vehicle/request/VehicleCreateRequestDTO.java
@@ -10,6 +10,7 @@ public record VehicleCreateRequestDTO(
     @NotBlank(message = "License plate is required")
     @Size(max = 20, message = "License plate must be at most 20 characters")
     String licensePlate,
+    @NotBlank(message = "Model is required")
     @Size(max = 100, message = "Model must be at most 100 characters")
     String model
 ) {}
